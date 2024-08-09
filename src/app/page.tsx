324bcd26003965cf7cc74144c435e3f9cfe69986
Artem <atremat@gmail.com>
2024-08-09 23:44:04 +0300

    add company button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
+import AddCompanyButton from '@/components/add-company-button';
 import StatusLabel, { Status } from '@/components/status-label';
+import { headers } from 'next/headers';
 
 export default function Home() {
+  console.log(headers());
+
   return (
     <main>
-      <h1 className="text-xl">Home page</h1>
-      <StatusLabel status={Status.Active}>Active</StatusLabel>
-      <StatusLabel status={Status.NotActive}>Not active</StatusLabel>
-      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
-      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
+      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <AddCompanyButton />
     </main>
   );
 }
