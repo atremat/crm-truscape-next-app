95fe5f03178e8e61a3ac5320ed94e2fac84e52e7
Artem <atremat@gmail.com>
2024-08-11 18:43:06 +0300

    Error Handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import AddCompanyButton from '@/app/components/add-company-button';
 import StatusLabel, { Status } from '@/app/components/status-label';
 import { headers } from 'next/headers';
+import MagicButton from './components/magic-button';
 
 export default function Home() {
   console.log(headers());
 
   return (
     <main>
-      <h1 className="text-xl">Home page {new Date().toTimeString()}</h1>
+      <h1 className="text-xl">Home page</h1>
       <AddCompanyButton />
+      <MagicButton />
     </main>
   );
 }
