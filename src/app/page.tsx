17f3a28188c59d2934c3dfb28e88e763e863608d
Artem <atremat@gmail.com>
2024-08-13 18:30:28 +0300

    react query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import AddCompanyButton from '@/app/components/add-company-button';
-import StatusLabel, { Status } from '@/app/components/status-label';
-import { headers } from 'next/headers';
-import MagicButton from './components/magic-button';
-
 export default function Home() {
-  console.log(headers());
-
   return (
     <main>
       <h1 className="text-xl">Home page</h1>
-      <AddCompanyButton />
-      <MagicButton />
     </main>
   );
 }
